fa680add7010f964f7864acb60ae5f651fb36e62
Axel7755 <baxeltomas@gmail.com>
2024-08-26 13:53:11 -0600

    cambios de pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LateralLogin} from './loging-regis/lateral.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,LateralLogin],
+  template: `<main class="main">   
+  <div class="content">
+  <lateral-login></lateral-login>
+  <div class="divider" role="separator" aria-label="Divider"></div>
+
+  </div>
+</main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/loging-regis/lateral.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'lateral-login',
+  standalone: true,
+  templateUrl: './lateral.component.html',
+  styleUrl: './lateral.component.css'
+})
+export class LateralLogin{
+    title = 'pivec';
+}
