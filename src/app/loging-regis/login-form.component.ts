ca65e23c163090f706769e04f6f5d9755db45032
Axel7755 <baxeltomas@gmail.com>
2024-08-28 14:27:45 -0600

    fornulario

--- src/app/loging-regis/login-form.component.ts
+++ src/app/loging-regis/login-form.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatInputModule,MatFormFieldModule,MatIconModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
