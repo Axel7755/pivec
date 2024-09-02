17a0d466931557146c83cf20fa6ffc039678de19
Axel7755 <baxeltomas@gmail.com>
2024-09-02 14:26:43 -0600

    cam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './loging-regis/login.component';
-import { RegistroPageComponent } from "./loging-regis/registro-page.component";
-import { LateralLogin } from "./loging-regis/sub-componentes/lateral.component";
 import { MenuPrincipalComponent } from "./menu-principal/menu-principal.component";
 
 @Component({
@@ -11,8 +9,8 @@ import { MenuPrincipalComponent } from "./menu-principal/menu-principal.componen
   standalone: true,
   imports: [CommonModule,
     RouterOutlet,
-    LoginComponent,
-    RegistroPageComponent, LateralLogin, MenuPrincipalComponent],
+    LoginComponent
+    ],
   template: `
   <router-outlet></router-outlet>
   `,
