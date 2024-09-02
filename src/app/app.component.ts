e1201ee30bcb1debb73b4c7cc90cfd73a7c84a52
Axel7755 <baxeltomas@gmail.com>
2024-09-02 14:57:34 -0600

    errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './loging-regis/login.component';
+import { MenuPrincipalComponent } from "./menu-principal/menu-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
     RouterOutlet,
-    LoginComponent
-    ],
+    LoginComponent, MenuPrincipalComponent],
   template: `
   <app-login></app-login>
+  <app-menu-principal></app-menu-principal>
   `,
   styleUrl: './app.component.css'
 })
