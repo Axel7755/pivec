f7bc1d1b3f7c9cb662a11eec591a8a37a2ea5792
Axel7755 <baxeltomas@gmail.com>
2024-09-02 14:58:52 -0600

    errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './loging-regis/login.component';
 import { MenuPrincipalComponent } from "./menu-principal/menu-principal.component";
+import { LoginComponent } from './loging-regis/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
-    RouterOutlet,
-    LoginComponent, MenuPrincipalComponent],
+    RouterOutlet, MenuPrincipalComponent, LoginComponent],
   template: `
+<<<<<<< HEAD
 <<<<<<< HEAD
   <router-outlet></router-outlet>
 =======
   <app-login></app-login>
   <app-menu-principal></app-menu-principal>
 >>>>>>> e1201ee (errores)
+=======
+  <app-menu-principal></app-menu-principal>
+  <app-login></app-login>
+>>>>>>> 4d05ec1 (errores)
   `,
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,14 @@ import { LoginFormComponent } from './loging-regis/sub-componentes/login-form.co
 import { RegistroComponent } from './loging-regis/sub-componentes/registro.component';
 
 export const routes: Routes = [
+
+    {path: 'menu-principal', component: MenuPrincipalComponent},
     {path: 'login-component', component: LoginFormComponent},
     {path: 'registro-component', component: RegistroComponent},
+<<<<<<< HEAD
     {path: 'menu-principal', component: MenuPrincipalComponent},
+=======
+    {path: 'verificar-datos', component:VerificarDatosComponent},
+>>>>>>> 4d05ec1 (errores)
     {path: '', redirectTo: '/login-component', pathMatch: 'full'}
 ];
