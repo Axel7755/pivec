3db7d710f458bc2bb5ce05b2179592347f22a0f1
Axel7755 <baxeltomas@gmail.com>
2024-09-02 14:19:24 -0600

    cambios redireciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuPrincipalComponent } from "./menu-principal/menu-principal.component";
 import { LoginComponent } from './loging-regis/login.component';
+import { RegistroPageComponent } from "./loging-regis/registro-page.component";
+import { LateralLogin } from "./loging-regis/sub-componentes/lateral.component";
+import { MenuPrincipalComponent } from "./menu-principal/menu-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
-    RouterOutlet, MenuPrincipalComponent, LoginComponent],
+    RouterOutlet,
+    LoginComponent,
+    RegistroPageComponent, LateralLogin, MenuPrincipalComponent],
   template: `
 <<<<<<< HEAD
 <<<<<<< HEAD
