bf5591a2c17a0b645cbd835e37342b2024ceeaeb
AlanRicardoCalderonMtz <AlanRicardoCM@gmail.com>
2024-09-04 14:29:18 -0600

    se creo la sección de materias

--- src/app/materias/materias.component.ts
+++ src/app/materias/materias.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-materias',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './materias.component.html',
-  styleUrl: './materias.component.css'
+  styleUrl: './materias.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MateriasComponent {
 
