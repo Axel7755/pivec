cfca4b0f2e0234a2d8d6bc6f9cb6e8d1c692f8ac
Axel7755 <baxeltomas@gmail.com>
2024-11-04 10:41:29 -0600

    cambio aarchivos en concreto

--- backend/server.js
+++ backend/server.js
@@ -18,51 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
-// Configuración de almacenamiento para Multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadDir = 'uploads/tareasF';
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir); // Crear la carpeta si no existe
-    }
-    cb(null, uploadDir); // Carpeta de almacenamiento
-  },
-  filename: function (req, file, cb) {
-    const baseName = file.originalname.split('.')[0];
-    const extension = path.extname(file.originalname);
-    let newFileName = `${baseName}${extension}`;
-    let filePath = path.join('uploads/tareasF', newFileName);
-    let version = 1;
-    while (fs.existsSync(filePath)) {
-      version++;
-      newFileName = `${baseName}-v${version}${extension}`;
-      filePath = path.join('uploads/tareasF', newFileName);
-    }
-    cb(null, newFileName);
-  }
-});
-
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif|pdf|docx|xlsx|doc|xls|ppt|pptx|odt|ods|odp|rar|zip/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-    if (extname && mimetype) {
-      return cb(null, true);
-    }
-    cb(new Error('Tipo de archivo no permitido'));
-  }
-});
-
-// Ruta para manejar la carga de archivos
-app.post('/api/upload', upload.single('file'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: 'No se ha subido ningún archivo' });
-  }
-  res.json({ message: 'Archivo subido exitosamente', file: req.file });
-});
-
 // Función para conectar con la base de datos con reintentos
 const connectWithRetry = () => {
   db.sequelize.sync()
