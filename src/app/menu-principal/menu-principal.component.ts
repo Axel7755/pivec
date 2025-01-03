40ae70891cba32b4f31d194c41e0ac88e449c0aa
AlanRicardoCalderonMtz <AlanRicardoCM@gmail.com>
2025-01-03 12:25:09 -0600

    En videos compartidos muestra el boton para subir videos, y se modificaron estilos. En verificar-datos falta mostrar la tabla.

--- src/app/menu-principal/menu-principal.component.ts
+++ src/app/menu-principal/menu-principal.component.ts
@@ -75,7 +75,7 @@ export class MenuPrincipalComponent implements OnInit {
     {
       icon: 'edit',
       name: 'Modificar horario',
-      route: ''
+      route: '/menu-principal/modificar-horario'
     },
   ];
 }
